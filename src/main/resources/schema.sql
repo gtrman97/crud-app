0596c814a78069503b88abb748ef5f0840b205e6
Andrew Killian <ack97@txstate.edu>
2024-03-29 17:08:55 -0500

    corrected schema and updated spring version

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,3 @@
-CREATE TABLE person (
-    person_id integer IDENTITY,
-    first_name varchar(50) NOT NULL,
-    last_name varchar(50) NOT NULL,
-    email_address varchar(50) NOT NULL,
-    street_address varchar(50) NOT NULL,
-    city varchar(50) NOT NULL,
-    state varchar(2) NOT NULL,
-    zip_code varchar(5) NOT NULL
-);
-
 CREATE TABLE client (
     client_id integer IDENTITY,
     company_name varchar(100) NOT NULL,
@@ -17,5 +6,15 @@ CREATE TABLE client (
     address varchar(100)
 );
 
-ALTER TABLE person ADD COLUMN client_id integer;
-ALTER TABLE person ADD FOREIGN KEY (client_id) REFERENCES client(client_id);
+CREATE TABLE person (
+    person_id integer IDENTITY,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    email_address varchar(50) NOT NULL,
+    street_address varchar(50) NOT NULL,
+    city varchar(50) NOT NULL,
+    state varchar(2) NOT NULL,
+    zip_code varchar(5) NOT NULL,
+    client_id integer,  -- to establish a relationship with the Client table
+    FOREIGN KEY (client_id) REFERENCES client(client_id)  -- to define the foreign key constraint
+);
